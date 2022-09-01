768033a79f882e6f358d482aaa624ed2ae5ce650
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-09-01 15:39:46 +0430

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("express")().listen(1343);
 const db = require("quick.db");
 const discord = require("discord.js");
 const client = new discord.Client({ disableEveryone: true });
-client.login("TOKEN");
+client.login(process.env.token);
 const fetch = require("node-fetch");
 const fs = require("fs");
 
